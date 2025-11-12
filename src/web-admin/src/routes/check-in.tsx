23ad67d45772b770880b6c829e65adb2093e5f84
hebah <hebahshamnad@gmail.com>
2025-11-12 08:54:06 -0500

    add dependencies

--- src/web-admin/src/routes/check-in.tsx
+++ src/web-admin/src/routes/check-in.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-
+// askshita and hebah
 export const Route = createFileRoute('/check-in')({
   component: RouteComponent,
 })
--- src/web-admin/src/routes/notifications.tsx
+++ src/web-admin/src/routes/notifications.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-
+// rachel & himasha
 export const Route = createFileRoute('/notifications')({
   component: RouteComponent,
 })
